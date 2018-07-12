b700ce78eb90b10133448dfa8a7f2b31aa907b91
Brandon Cunningham <brandido@gmail.com>
2018-07-11 21:56:36 -0600

    Refactor all files, add bookmark counter and clear inputs after submiting data

--- scripts.js
+++ scripts.js
@@ -4,12 +4,13 @@ var enterButton = document.querySelector('.enter-button');
 var readButton = document.querySelector('.read-button');
 var deleteButton = document.querySelector('.delete-button');
 var bookmarksUl = document.querySelector('ul');
+var linkCounterDiv = document.querySelector('.link-counter');
 var bookmarksArray = [];
 
 websiteTitleInput.addEventListener('keyup', enableButton);
 websiteUrlInput.addEventListener('keyup', enableButton);
 enterButton.addEventListener('click', createBookmark);
-bookmarksUl.addEventListener('click', styleReadButton); 
+bookmarksUl.addEventListener('click', styleReadDeleteButtons); 
 bookmarksUl.addEventListener('click', deleteButton); 
 
 function addBookmarkData() {
@@ -19,25 +20,35 @@ function addBookmarkData() {
   });
 };
 
+function enableButton() {
+  if (websiteTitleInput.value.length * websiteUrlInput.value.length > 0) {
+    enterButton.removeAttribute('disabled');  
+  }
+}
+
 function createBookmark(e) {
   e.preventDefault();
   if (websiteTitleInput.value.length * websiteUrlInput.value.length === 0) {
     alert('Yo! Please enter both a website title and webstire url.');
   } else {
     addBookmarkData();
-  var newLi = document.createElement('li');
-  newLi.innerHTML = 
-    `<section class="bookmark">
-          <h2 class="website-title">${websiteTitleInput.value}</h2>
-          <a href="${websiteUrlInput.value}" target="_blank"><h3>${websiteUrlInput.value}</h3></a>
-            <button class="read-button">Read</button>
-            <button class="delete-button">Delete</button>         
-      </section>`;
-  bookmarksUl.appendChild(newLi);
+    var newLi = document.createElement('li');
+    newLi.innerHTML = 
+      `<section class="bookmark">
+            <h2 class="website-title">${websiteTitleInput.value}</h2>
+            <a href="${websiteUrlInput.value}" target="_blank"><h3>${websiteUrlInput.value}</h3></a>
+              <button class="read-button">Read</button>
+              <button class="delete-button">Delete</button>         
+        </section>`;
+    bookmarksUl.appendChild(newLi);
+    websiteTitleInput.value = '';
+    websiteUrlInput.value = '';
+    linkCounter();
+    deleteButton.addEventListener('click', linkCounter);       
     };
   };
 
-function styleReadButton() {
+function styleReadDeleteButtons() {
   var buttonClicked = event.target;
   var li = event.target.parentNode.parentNode;
   if (buttonClicked.className === 'read-button') {
@@ -52,8 +63,11 @@ function styleReadButton() {
   }
 };
 
-function enableButton() {
-  if (websiteTitleInput.value.length * websiteUrlInput.value.length > 0) {
-    enterButton.removeAttribute('disabled');  
-  }
+function linkCounter() {
+  var linkNumber = bookmarksArray.length;
+  // var counter = document.createElement('h4');
+  linkCounterDiv.innerText = `You have ${linkNumber} bookmark(s).`;
+  console.log(linkNumber);
 }
+
+
